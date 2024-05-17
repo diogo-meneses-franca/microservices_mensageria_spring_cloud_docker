622bbbacdf67fb6b53dae5d8451891b8436619ee
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-05-17 20:07:45 -0300

    feat: mscloudgateway

--- msclientes/src/main/java/com/pbcompass/msclientes/controller/ClienteController.java
+++ msclientes/src/main/java/com/pbcompass/msclientes/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.pbcompass.msclientes.dto.ClienteCadastrarDto;
 import com.pbcompass.msclientes.entity.Cliente;
 import com.pbcompass.msclientes.service.ClienteService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,6 +14,7 @@ import java.net.URI;
 @RestController
 @RequestMapping("/clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClienteController {
 
     private final ClienteService service;
